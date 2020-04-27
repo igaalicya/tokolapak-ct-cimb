649bc978a5e51a2893a46250021cba384676b638
igaalicya <igaalicya2@gmail.com>
2020-04-27 14:34:06 +0700

    json

--- src/views/screens/Auth/AuthScreen.jsx
+++ src/views/screens/Auth/AuthScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TextField from "../../components/TextField/TextField";
 import ButtonUI from "../../components/Button/Button";
+import "./AuthScreen.css";
 
 class AuthScreen extends React.Component {
   render() {
@@ -8,19 +9,32 @@ class AuthScreen extends React.Component {
       <div className="container">
         <div className="row mt-5">
           <div className="col-5">
+            <div className="d-flex justify-content-start">
+              <ButtonUI type="light-pill-unselected" className="mt-4">
+                Register
+              </ButtonUI>
+              <ButtonUI type="light-pill-selected" className="mt-4 ml-2">
+                Login
+              </ButtonUI>
+            </div>
             <div>
-              <h3>Login</h3>
-              <p className="mt-4 text-secondary">
+              <h3 className="mt-4">Login</h3>
+              <p className="mt-4 text-gray">
                 Welcome back.
                 <br />
                 Please, login to your account
               </p>
               <TextField placeholder="Username" className="mt-5" />
               <TextField placeholder="Password" className="mt-2" />
+              {/* <p className="rect"></p> */}
+              <p className="mt-2">Remember me</p>
               <div className="d-flex justify-content-center">
                 <ButtonUI type="contained" className="mt-4">
                   Login
                 </ButtonUI>
+              </div>
+              <div className="d-flex justify-content-center">
+                <p className="text-accent-default mt-3">Forgot Password?</p>
               </div>
             </div>
           </div>
