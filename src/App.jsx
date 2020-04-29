2725f80a922a7ea8fca1140b423620e9d4684a6e
igaalicya <igaalicya2@gmail.com>
2020-04-29 11:25:11 +0700

    added product details

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Home from "./views/screens/Home/Home";
 import Navbar from "./views/components/Navbar/Navbar";
 import AuthScreen from "./views/screens/Auth/AuthScreen";
 import { userKeepLogin } from "./redux/actions";
+import ProductDetails from "./views/screens/ProductDetails/ProductDetails";
 
 const cookieObj = new Cookie();
 
@@ -28,6 +29,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/auth" component={AuthScreen} />
+          <Route exact path="/product/:id" component={ProductDetails} />
         </Switch>
         <div style={{ height: "120px" }} />
       </>
@@ -35,14 +37,14 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    user: state.user,
+    user: state.user
   };
 };
 
 const mapDispatchToProps = {
-  keepLogin: userKeepLogin,
+  keepLogin: userKeepLogin
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(App));
